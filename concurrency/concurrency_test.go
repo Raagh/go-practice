819da702cf405de3a508e947a17c5afe1eb7a5c8
Raagh <pattferraggi@gmail.com>
2025-02-24 22:11:46 +0100

    testing structs and basic concurrency

---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,18 @@
+package main
+
+type TestRun struct {
+	value int
+	want  bool
+}
+
+// func TestIsPrime(t *testing.T) {
+// 	for _, v := range []TestRun{
+// 		{value: 2, want: true},
+// 		{value: 4, want: false},
+// 		{value: 17, want: true},
+// 	} {
+// 		if observed := IsPrime(v.value); observed != v.want {
+// 			t.Fatalf("IsPrime(%d) = %v, want %v", v.value, observed, v.want)
+// 		}
+// 	}
+// }
