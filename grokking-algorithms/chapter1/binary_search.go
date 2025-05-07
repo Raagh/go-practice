37954cb11834d5327a3aa2d4c51bfb5642652a5e
Raagh <pattferraggi@gmail.com>
2025-05-07 08:36:57 +0200

    rework chapter 1 and 2 from scratch, do I still remember?

--- grokking-algorithms/chapter1/binary_search.go
+++ grokking-algorithms/chapter1/binary_search.go
@@ -4,16 +4,16 @@ func BinarySearch(array []int, num int) int {
 	low := 0
 	high := len(array) - 1
 
-	for low < high {
-		mid := low + high/2
+	for low <= high {
+		mid := (low + high) / 2
 		guess := array[mid]
 
-		if num == guess {
+		if guess == num {
 			return mid
-		} else if num < guess {
-			high = mid
+		} else if guess < num {
+			low = mid + 1
 		} else {
-			low = mid
+			high = mid - 1
 		}
 	}
 
--- grokking-algorithms/chapter2/selection_sort.go
+++ grokking-algorithms/chapter2/selection_sort.go
@@ -4,17 +4,19 @@ import "slices"
 
 func SelectionSort(array []int) []int {
 	new := make([]int, len(array))
-	for i := range len(array) {
-		smallest := findSmallest(array)
-		smallestElement := array[smallest]
-		array = slices.Delete(array, smallest, smallest+1)
+
+	for i := range array {
+		smallestIndex := findSmallestIndex(array)
+		smallestElement := array[smallestIndex]
+
+		array = slices.Delete(array, smallestIndex, smallestIndex+1)
 		new[i] = smallestElement
 	}
 
 	return new
 }
 
-func findSmallest(array []int) int {
+func findSmallestIndex(array []int) int {
 	smallest := array[0]
 	smallest_index := 0
 	for i, v := range array {
