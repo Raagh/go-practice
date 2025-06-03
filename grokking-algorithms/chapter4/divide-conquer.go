a07838b62ff3fc7c4606735b2169baf88cfae193
Raagh <pattferraggi@gmail.com>
2025-06-03 17:16:52 +0200

    grokking algorithms rework D&C

--- grokking-algorithms/chapter4/divide-conquer.go
+++ grokking-algorithms/chapter4/divide-conquer.go
@@ -23,30 +23,35 @@ func Max(items []int) int {
 		return items[0]
 	} else {
 		maxRest := Max(items[1:])
+
 		if items[0] > maxRest {
 			return items[0]
+		} else {
+			return maxRest
 		}
-		return maxRest
 	}
 }
 
 func BinarySearch(array []int, n int) int {
 	if len(array) == 0 {
 		return -1
-	}
-
-	mid := len(array) / 2
-	guess := array[mid]
-
-	if guess == n {
-		return mid
-	} else if guess > n {
-		return BinarySearch(array[:mid], n)
 	} else {
-		res := BinarySearch(array[mid+1:], n)
-		if res == -1 {
-			return -1
+
+		mid := len(array) / 2
+		guess := array[mid]
+
+		if guess == n {
+			return mid
+		} else if guess > n {
+			return BinarySearch(array[:mid], n)
+		} else {
+			res := BinarySearch(array[mid+1:], n)
+
+			if res == -1 {
+				return -1
+			} else {
+				return mid + 1 + res
+			}
 		}
-		return mid + 1 + res
 	}
 }
