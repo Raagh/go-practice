adf7e5dc7a826d7c8407bb96281c809c1b69cf8a
Raagh <pattferraggi@gmail.com>
2025-06-03 18:03:11 +0200

    grokking algorithms rework quicksort

--- grokking-algorithms/chapter4/quicksort.go
+++ grokking-algorithms/chapter4/quicksort.go
@@ -9,24 +9,27 @@ func QuickSort(arr []int) []int {
 	if len(arr) < 2 {
 		return arr
 	}
+
 	pivotIndex := rand.IntN(len(arr))
 	pivot := arr[pivotIndex]
 
-	withoutPivot := slices.Delete(arr, pivotIndex, pivotIndex+1)
-	less, greater := getSmallerAndLarger(withoutPivot, pivot)
+	withoutResults := slices.Delete(arr, pivotIndex, pivotIndex+1)
+	smaller, larger := getSmallerAndLarger(withoutResults, pivot)
 
-	return append(append(QuickSort(less), pivot), QuickSort(greater)...)
+	return append(append(QuickSort(smaller), pivot), QuickSort(larger)...)
 }
 
 func getSmallerAndLarger(array []int, pivot int) (smaller []int, larger []int) {
 	smaller = []int{}
 	larger = []int{}
+
 	for _, v := range array {
-		if v <= pivot {
+		if v < pivot {
 			smaller = append(smaller, v)
 		} else {
 			larger = append(larger, v)
 		}
 	}
+
 	return smaller, larger
 }
